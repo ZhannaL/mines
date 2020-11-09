db83507bedb3a3d36016b72e9e0fca4b612f298b
ZhannaL <zhannet1814@gmail.com>
2020-11-09 15:36:45 +0100

    feat: added metadata helmet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,18 @@ const IndexPage = (): JSX.Element => {
   return (
     <>
       <Helmet defer={false}>
+        <html lang="en" />
+        <title itemProp="name" lang="en">
+          Mine App
+        </title>
         <meta charSet="utf-8" />
         <link rel="icon" href="/src/images/mine-icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <title>Mines App</title>
-        <meta name="description" content="Mines application" />
+        <meta name="description" content="Mine App" />
+        <base target="_blank" href="https://mines-react.netlify.app/" />
+        <link rel="canonical" href="https://mines-react.netlify.app/" />
+        <noscript>You need to enable JavaScript to run this app.</noscript>
       </Helmet>
       <HomePage />
     </>
